9ba15d845b719039fb6c9632341c3facea0167c5
dbaier <dieter.baier@novatec-gmbh.de>
2013-07-04 07:50:54 +0200

    some more clean code

--- src/main/java/dieterbaier/kata/args/ArgumentParser.java
+++ src/main/java/dieterbaier/kata/args/ArgumentParser.java
@@ -13,68 +13,62 @@ public class ArgumentParser {
     arguments.put(key, argument);
   }
 
-  public void addFlag(final char key, final boolean defaultValue, final boolean defaultValueForSetFlag) {
-    arguments.put(key, new BooleanArgument(defaultValue, defaultValueForSetFlag));
-  }
-
-  public void addFlag(final char key, final Integer defaultValue, final Integer defaultValueForSetFlag) {
-    arguments.put(key, new IntegerArgument(defaultValue, defaultValueForSetFlag));
-  }
-
-  public void addFlag(final char key, final String defaultValue, final String defaultValueForSetFlag) {
-    arguments.put(key, new StringArgument(defaultValue, defaultValueForSetFlag));
-  }
-
   public String getValue(final char key) {
     return arguments.get(key).getValueAsString();
   }
 
   public void parse(final String[] args) {
+    // we need to use this kind of for-loop because in case one argument belongs to the other argument, the loop must
+    // skip this argument
     for (int indexOfCurrentArgument = 0; indexOfCurrentArgument < args.length; indexOfCurrentArgument++) {
       final String potentialFlag = args[indexOfCurrentArgument];
-      final String nextGivenArgument = isOneMoreArgumentAvailable(args, indexOfCurrentArgument) ? getNextArgument(args, indexOfCurrentArgument) : null;
+      final String nextGivenArgument = isOneMoreArgumentAvailable(args, indexOfCurrentArgument) ? getNextArgument(args,
+          indexOfCurrentArgument) : null;
       final char key = isFlag(potentialFlag) ? getFlagValue(potentialFlag) : ' ';
       final boolean isNextArgumentIsPartOfGivenArgument = setValueForCurrentFlag(key, nextGivenArgument);
       if (isNextArgumentIsPartOfGivenArgument)
+        // we skip the next argument, since it belonged to the current one and should not be handled by the loop
+        // therefore
         indexOfCurrentArgument++;
     }
+  }
+
+  private Argument<?> getAllreadySetArgument(final char key) {
+    if (!arguments.keySet().contains(key))
+      throw new IllegalArgumentException("Illegal flag" + key);
+    return arguments.get(key);
   }
 
   private char getFlagValue(final String potentialFlag) {
     return potentialFlag.charAt(1);
   }
 
-  private String getNextArgument(final String[] args, int indexOfCurrentArgument) {
+  private String getNextArgument(final String[] args, final int indexOfCurrentArgument) {
     return args[indexOfCurrentArgument + 1];
-  }
-
-  private boolean isOneMoreArgumentAvailable(final String[] args, int indexOfCurrentArgument) {
-    return indexOfCurrentArgument < args.length - 1;
-  }
-
-  private Argument<?> getAllreadySetArgument(final char key) {
-    if (!arguments.keySet().contains(key))
-      throw new IllegalArgumentException("Illegal flag" + key);
-    final Argument<?> currentArgument = arguments.get(key);
-    return currentArgument;
   }
 
   private boolean isFlag(final String argument) {
     return argument != null && argument.length() == 2 && argument.startsWith("-");
   }
 
+  private boolean isNextArgumentPartOfThisArgument(final String nextGivenArgument) {
+    return nextGivenArgument != null && !isFlag(nextGivenArgument);
+  }
+
+  private boolean isOneMoreArgumentAvailable(final String[] args, final int indexOfCurrentArgument) {
+    return indexOfCurrentArgument < args.length - 1;
+  }
+
   private boolean setValueForCurrentFlag(final char key, final String nextGivenArgument) {
     final Argument<?> currentArgument = getAllreadySetArgument(key);
-    String value;
-    boolean isNextArgumentIsPartOfGivenArgument = false;
-    if (nextGivenArgument == null || isFlag(nextGivenArgument))
-      value = currentArgument.getDefaultValueForSetFlag();
-    else {
-      value = nextGivenArgument;
-      isNextArgumentIsPartOfGivenArgument = true;
+    if (isNextArgumentPartOfThisArgument(nextGivenArgument)) {
+      currentArgument.setValue(nextGivenArgument);
+      return true;
     }
-    currentArgument.setValue(value);
-    return isNextArgumentIsPartOfGivenArgument;
+    else {
+      currentArgument.setValue(currentArgument.getDefaultValueForSetFlag());
+      return false;
+    }
   }
 
 }
