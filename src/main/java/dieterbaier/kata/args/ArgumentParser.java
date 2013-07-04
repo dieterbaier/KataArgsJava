c4ff971ce19f45b7575651ac3460dd78e42620f3
dbaier <dieter.baier@novatec-gmbh.de>
2013-07-04 14:50:47 +0200

    Refactored some hardcoded values to constants

--- src/main/java/dieterbaier/kata/args/ArgumentParser.java
+++ src/main/java/dieterbaier/kata/args/ArgumentParser.java
@@ -10,6 +10,22 @@ import java.util.regex.Pattern;
 
 public class ArgumentParser {
 
+  private static final String               CLOSINGBRACKET   = "\\]";
+
+  private static final String               OPENINGBRACKET   = "\\[";
+
+  private static final String               MINUSSIGN        = "-";
+
+  private static final String               COMMA            = ",";
+
+  private static final String               BLANK            = " ";
+
+  private static final String               NOTHING          = "";
+
+  private static final String               ANYCHARACTERS    = ".*";
+
+  private static final String               APOSTROPHE       = "\"";
+
   private static final String               BLANKPLACEHOLDER = "%20";
 
   private final Map<Character, Argument<?>> allreadySetFlags = new HashMap<Character, Argument<?>>();
@@ -21,23 +37,22 @@ public class ArgumentParser {
   }
 
   public String getValue(final char flag) {
-    return Arrays.toString(allreadySetFlags.get(flag).getValue()).replaceAll("\\[", "").replaceAll("\\]", "")
-        .replaceAll(", ", ",");
+    return removeUnnaccassaryCharsFromArrayRepresantation(Arrays.toString(getFlagsArgumentValue(flag)));
   }
 
   public String[] getValues(final char flag) {
-    return getValue(flag).split(",");
+    return getValue(flag).split(COMMA);
   }
 
   public void parse(final String argumentList) {
     String manipulatedList = argumentList;
-    final Matcher parser = Pattern.compile("\".*\"").matcher(argumentList);
+    final Matcher parser = Pattern.compile(APOSTROPHE + ANYCHARACTERS + APOSTROPHE).matcher(argumentList);
     while (parser.find()) {
       final String originalText = parser.group();
-      final String newText = originalText.replaceAll("\"", "").replaceAll(" ", BLANKPLACEHOLDER);
+      final String newText = originalText.replaceAll(APOSTROPHE, NOTHING).replaceAll(BLANK, BLANKPLACEHOLDER);
       manipulatedList = manipulatedList.replace(originalText, newText);
     }
-    parse(manipulatedList.split(" "));
+    parse(manipulatedList.split(BLANK));
   }
 
   public void parse(final String[] args) {
@@ -48,7 +63,7 @@ public class ArgumentParser {
       final String potentialFlag = args[indexOfCurrentArgument];
       final String nextGivenArgument = isOneMoreArgumentAvailable(args, indexOfCurrentArgument) ? getNextArgument(args,
           indexOfCurrentArgument) : null;
-      final char flagsKey = isFlag(potentialFlag) ? getFlagsKey(potentialFlag) : ' ';
+      final char flagsKey = isFlag(potentialFlag) ? getFlagsKey(potentialFlag) : BLANK.charAt(0);
       checkIfFlagWasAllreadyHandled(allreadyHandledFlags, flagsKey);
       if (isNextArgumentIsPartOfGivenFlag(getAllreadySetFlag(flagsKey), nextGivenArgument))
         // we skip the next argument, since it belonged to the current one and should not be handled by the loop
@@ -62,6 +77,10 @@ public class ArgumentParser {
       throw new IllegalArgumentException("Flag " + currentFlagToHandle + " twice in the argument-list");
     else
       allreadyHandledFlags.add(currentFlagToHandle);
+  }
+
+  private Object[] getFlagsArgumentValue(final char flag) {
+    return allreadySetFlags.get(flag).getValue();
   }
 
   private Argument<?> getAllreadySetFlag(final char flag) {
@@ -79,7 +98,7 @@ public class ArgumentParser {
   }
 
   private boolean isFlag(final String argument) {
-    return argument != null && argument.length() == 2 && argument.startsWith("-")
+    return argument != null && argument.length() == 2 && argument.startsWith(MINUSSIGN)
         && allreadySetFlags.containsKey(getFlagsKey(argument));
   }
 
@@ -89,9 +108,9 @@ public class ArgumentParser {
 
   private boolean isNextArgumentIsPartOfGivenFlag(final Argument<?> argumentOfGivenFlag, final String nextArgument) {
     if (isNextArgumentAFlagsArgument(nextArgument)) {
-      final String[] values = nextArgument.split(",");
+      final String[] values = nextArgument.split(COMMA);
       for (final String value : values)
-        argumentOfGivenFlag.setValue(value.replaceAll(BLANKPLACEHOLDER, " "));
+        argumentOfGivenFlag.setValue(value.replaceAll(BLANKPLACEHOLDER, BLANK));
       return true;
     }
     else {
@@ -102,6 +121,12 @@ public class ArgumentParser {
 
   private boolean isOneMoreArgumentAvailable(final String[] args, final int indexOfCurrentArgument) {
     return indexOfCurrentArgument < args.length - 1;
+  }
+
+  private String removeUnnaccassaryCharsFromArrayRepresantation(final String arrayAsString) {
+    return arrayAsString.replaceAll(OPENINGBRACKET, NOTHING)
+        .replaceAll(CLOSINGBRACKET, NOTHING)
+        .replaceAll(COMMA + BLANK, COMMA);
   }
 
 }
